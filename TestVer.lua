a923b23b4b0aaae0a370e829624bac4e0e3615b6
Icesythe7 <lucasloveless90@gmail.com>
2017-11-09 20:42:37 -0500

    init

--- TestVer.lua
+++ TestVer.lua
@@ -2,7 +2,7 @@ local version = 0.91
 local check = "https://raw.githubusercontent.com/Icesythe7/PokeOne/master/TestVer.version"
 local file = "https://raw.githubusercontent.com/Icesythe7/PokeOne/master/TestVer.lua"
 
-script.downloadFile(file, script.path.."/blah2.lua", check, version)
+script.downloadFile(file, script.path.."/TestVer.lua", check, version)
 
 function onPathAction()
 end
