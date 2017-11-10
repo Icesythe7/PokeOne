777f4033e950054beb682811e2261c18df3d4875
Icesythe7 <lucasloveless90@gmail.com>
2017-11-09 20:55:31 -0500

    update test

--- TestVer.lua
+++ TestVer.lua
@@ -1,8 +1,11 @@
-local version = 0.91
+local autoUpdate = true --can be changed by user incase issues with updater
+local version = 0.92
 local check = "https://raw.githubusercontent.com/Icesythe7/PokeOne/master/TestVer.version"
 local file = "https://raw.githubusercontent.com/Icesythe7/PokeOne/master/TestVer.lua"
 
-script.downloadFile(file, script.path.."/TestVer.lua", check, version)
+if autoUpdate then
+	script.downloadFile(file, script.path.."/TestVer.lua", check, version)
+end
 
 function onPathAction()
 end
