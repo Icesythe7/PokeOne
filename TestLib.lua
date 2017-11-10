89ca31df3827510ea03a165326b7fbfdc512c4ad
Icesythe7 <lucasloveless90@gmail.com>
2017-11-09 22:13:30 -0500

    remove slash

--- TestLib.lua
+++ TestLib.lua
@@ -2,6 +2,6 @@ local version = 0.23
 local check = "https://raw.githubusercontent.com/Icesythe7/PokeOne/master/TestLib.version"
 local file = "https://raw.githubusercontent.com/Icesythe7/PokeOne/master/TestLib.lua"
 
-if script.update(file, script.libPath.."/TestLib.lua", check, version) then return end
+if script.update(file, script.libPath.."TestLib.lua", check, version) then return end
 
 print("OMFG it downloaded the lib automatically!")
--- TestVer.lua
+++ TestVer.lua
@@ -2,14 +2,14 @@ local version = 0.96
 local check = "https://raw.githubusercontent.com/Icesythe7/PokeOne/master/TestVer.version"
 local file = "https://raw.githubusercontent.com/Icesythe7/PokeOne/master/TestVer.lua"
 
-if script.exists(script.libPath.. "/TestLib.lua") then
+if script.exists(script.libPath.. "TestLib.lua") then
 	require "TestLib"
 else
-	script.downloadFile("https://raw.githubusercontent.com/Icesythe7/PokeOne/master/TestLib.lua", script.libPath.. "/TestLib.lua")
+	script.downloadFile("https://raw.githubusercontent.com/Icesythe7/PokeOne/master/TestLib.lua", script.libPath.. "TestLib.lua")
 	require "TestLib"
 end
 
-if script.update(file, script.path.."/TestVer.lua", check, version, true) then return end
+if script.update(file, script.path.."TestVer.lua", check, version, true) then return end
 
 function onPathAction()
 end
